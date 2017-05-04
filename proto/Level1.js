7e02932abad5ccfc1ec6e36c17758c813348a242
Eric Posas <ericposas06@gmail.com>
2017-05-04 15:01:56 -0500

    rewrite Keys.js

--- proto/Level1.js
+++ proto/Level1.js
@@ -1,11 +1,11 @@
 // DEFINE LVL 1 HERE 
 
 function Level1(props){
-  var boxA = Matter.Bodies.rectangle(280, 200, 40, 40);
+  var boxA = Build(280, 200, 'box1', 'box-a');
   var boxB = Matter.Bodies.rectangle(300, 50, 20, 20);
   var ground = Matter.Bodies.rectangle(400, props.h, 810, 60, { isStatic: true });
-  var platA = Build(220, 280, 'A');
-  var platA2 = Build(60, 440, 'A');
+  var platA = Build(220, 280, 'platform1', 'platform-a');
+  var platA2 = Build(60, 440, 'platform2', 'platform-a');
   var bodies = [boxA, boxB, ground, platA, platA2];
   return bodies;
 }
--- utils/Build.js
+++ utils/Build.js
@@ -2,10 +2,16 @@
 function Build(x, y, id, type){
   var bod;
   switch(type){
-    case 'A':
+    case 'platform-a':
       bod = Matter.Bodies.rectangle(x, y, 140, 40, {
         id: id,
         isStatic: true
+      });
+      return bod;
+      break;
+    case 'box-a':
+      bod = Matter.Bodies.rectangle(x, y, 40, 40, {
+      id: id
       });
       return bod;
       break;
