342d6af9b21170acfba8c8b0fd42ab3b7aad1882
Eric Posas <ericposas06@gmail.com>
2017-05-04 15:05:04 -0500

    page.js now updates title and h3 element on page via same variable

--- js/page.js
+++ js/page.js
@@ -1,9 +1,10 @@
 var page = {
   properties : {},
-  setup_gamepage : function(t){
+  setup_gamepage : function(){
     var title = document.createElement('h3');
     title.id = 'title';
     title.innerHTML = page.properties.title;
     document.body.appendChild(title);
+    document.getElementsByTagName('title')[0].innerHTML = page.properties.title;
   }
 }
